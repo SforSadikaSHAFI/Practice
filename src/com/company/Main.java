59e5c5b5e448473c1595051ae4ea857dab13799c
SforSadikaSHAFI <medicaregrp7@gmail.com>
2022-01-05 15:27:33 +0600

    Finding even and odd

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,10 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         PracticeMethods p1 = new PracticeMethods();
-        System.out.println("Finding Average ...");
-        p1.findAvg();
-        System.out.println("Swapping two variables..");
-        p1.Swap();
+//        System.out.println("Finding Average ...");
+//        p1.findAvg();
+//        System.out.println("Swapping two variables..");
+//        p1.swap();
+        p1.findOddEven();
     }
 }
--- src/com/company/PracticeMethods.java
+++ src/com/company/PracticeMethods.java
@@ -7,11 +7,12 @@ public class PracticeMethods {
 
 //Write a Java program that takes five numbers as input to calculate and print the average of the numbers.
 
-    public PracticeMethods(){
+    public PracticeMethods() {
 
     }
-    public void findAvg(){
-        double a,b,c,d,e, avg, sum;
+
+    public void findAvg() {
+        double a, b, c, d, e, avg, sum;
 
         Scanner input = new Scanner(System.in);
         //double number ;
@@ -26,16 +27,16 @@ public class PracticeMethods {
         System.out.print("Enter 5th number :");
         e = input.nextDouble();
 
-        sum = a+b+c+d+e;
-        avg = sum/5;
-        System.out.println("Average of 5 numbers: "+avg);
+        sum = a + b + c + d + e;
+        avg = sum / 5;
+        System.out.println("Average of 5 numbers: " + avg);
 
 
     }
 
     //Write a Java program to swap two variables
-    public void Swap(){
-        int a,b,temp;
+    public void swap() {
+        int a, b, temp;
         Scanner input = new Scanner(System.in);
         System.out.print("Enter 1st number :");
         a = input.nextInt();
@@ -43,13 +44,45 @@ public class PracticeMethods {
         b = input.nextInt();
         System.out.println();
         System.out.println("***Before Swapping***");
-        System.out.println("a = "+ a +" b= "+b);
+        System.out.println("a = " + a + " b= " + b);
         temp = a;
         a = b;
         b = temp;
         System.out.println();
         System.out.println("***After Swapping***");
-        System.out.println("a = "+a +" b = "+b);
+        System.out.println("a = " + a + " b = " + b);
+    }
 
+    //    Write 2 methods  to print the odd and even  numbers from 1 to n
+    public void findOddEven() {
+        int n;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number :");
+        n = input.nextInt();
+        System.out.println();
+        oddNum(n);
+        System.out.println();
+        evenNum(n);
+    }
+
+
+    public void oddNum(int n) {
+        System.out.print("Odd numbers are :");
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) {
+                System.out.print(i + " ");
+            }
+
+        }
+    }
+
+    public void evenNum(int n) {
+        System.out.print("Even numbers are :");
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                    System.out.print(i + " ");
+            }
+
+        }
     }
 }
